3f62cb6db66a095ad80adb8deba45a625b06407b
ghost5554 <nick555310@gmail.com>
2024-09-01 12:52:25 +0300

    title update

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -4,8 +4,8 @@ const Home = () => {
     return (
         <>
             <div className="flex flex-col items-start justify-center gap-4 text-center w-[98vw] h-[150px] dark:text-white border-b-2">
-                <h1 className="text-5xl">cards page</h1>
-                <p className="text-3xl ">here you can find some cards</p>
+                <h1 className="text-5xl">Cards Page</h1>
+                <p className="text-3xl ">Here you can find some cards</p>
             </div>
             <Card />
         </>
