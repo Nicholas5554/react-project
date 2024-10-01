fc62718bee41ce226f7d1d048e5863add112d78b
ghost5554 <nick555310@gmail.com>
2024-10-01 21:39:21 +0300

    add error styling to CreateCard

--- src/Pages/CreateCard/CreateCard.tsx
+++ src/Pages/CreateCard/CreateCard.tsx
@@ -19,130 +19,158 @@ const CreateCard = () => {
                 <h1 className="text-2xl font-bold dark:text-white">Create card</h1>
 
                 <div className="flex flex-row gap-6 m-auto w-max">
-                    <FloatingLabel className="dark:text-white"
-                        type="text"
-                        variant="standard"
-                        label="title"
-                        {...register("title")}
-                    />
-                    <span className="text-sm text-red-500">{errors.title?.message}</span>
+                    <div className="flex flex-col w-[100%]">
+                        <FloatingLabel className="dark:text-white"
+                            type="text"
+                            variant="standard"
+                            label="title"
+                            {...register("title")}
+                        />
+                        <span className="text-sm text-red-500">{errors.title?.message}</span>
+                    </div>
 
-                    <FloatingLabel className="dark:text-white"
-                        type="text"
-                        variant="standard"
-                        label="subtitle"
-                        {...register("subtitle")}
-                    />
-                    <span className="text-sm text-red-500">{errors.subtitle?.message}</span>
+                    <div className="flex flex-col w-[100%]">
+                        <FloatingLabel className="dark:text-white"
+                            type="text"
+                            variant="standard"
+                            label="subtitle"
+                            {...register("subtitle")}
+                        />
+                        <span className="text-sm text-red-500">{errors.subtitle?.message}</span>
+                    </div>
                 </div>
 
                 <div className="flex flex-row gap-6 m-auto w-max">
-                    <FloatingLabel className="dark:text-white"
-                        type="text"
-                        variant="standard"
-                        label="description"
-                        {...register("description")}
-                    />
-                    <span className="text-sm text-red-500">{errors.description?.message}</span>
+                    <div className="flex flex-col w-[100%]">
+                        <FloatingLabel className="dark:text-white"
+                            type="text"
+                            variant="standard"
+                            label="description"
+                            {...register("description")}
+                        />
+                        <span className="text-sm text-red-500">{errors.description?.message}</span>
+                    </div>
 
-                    <FloatingLabel className="dark:text-white"
-                        type="text"
-                        variant="standard"
-                        label="phone"
-                        {...register("phone")}
-                    />
+                    <div className="flex flex-col w-[100%]">
+                        <FloatingLabel className="dark:text-white"
+                            type="text"
+                            variant="standard"
+                            label="phone"
+                            {...register("phone")}
+                        />
 
-                    <span className="text-sm text-red-500">{errors.phone?.message}</span>
+                        <span className="text-sm text-red-500">{errors.phone?.message}</span>
+                    </div>
                 </div>
 
                 <div className="flex flex-row gap-6 m-auto w-max">
-                    <FloatingLabel className="dark:text-white"
-                        type="text"
-                        variant="standard"
-                        label="email"
-                        {...register("email")}
-                    />
-                    <span className="text-sm text-red-500">{errors.email?.message}</span>
+                    <div className="flex flex-col w-[100%]">
+                        <FloatingLabel className="dark:text-white"
+                            type="text"
+                            variant="standard"
+                            label="email"
+                            {...register("email")}
+                        />
+                        <span className="text-sm text-red-500">{errors.email?.message}</span>
+                    </div>
 
-                    <FloatingLabel className="dark:text-white"
-                        type="text"
-                        variant="standard"
-                        label="web"
-                        {...register("web")}
-                    />
-                    <span className="text-sm text-red-500">{errors.web?.message}</span>
+                    <div className="flex flex-col w-[100%]">
+                        <FloatingLabel className="dark:text-white"
+                            type="text"
+                            variant="standard"
+                            label="web"
+                            {...register("web")}
+                        />
+                        <span className="text-sm text-red-500">{errors.web?.message}</span>
+                    </div>
                 </div>
 
                 <div className="flex flex-row gap-6 m-auto w-max">
-                    <FloatingLabel className="dark:text-white"
-                        type="text"
-                        variant="standard"
-                        label="image url"
-                        {...register("image.url")}
-                    />
-                    <span className="text-sm text-red-500">{errors.image?.url?.message}</span>
+                    <div className="flex flex-col w-[100%]">
+                        <FloatingLabel className="dark:text-white"
+                            type="text"
+                            variant="standard"
+                            label="image url"
+                            {...register("image.url")}
+                        />
+                        <span className="text-sm text-red-500">{errors.image?.url?.message}</span>
+                    </div>
 
-                    <FloatingLabel className="dark:text-white"
-                        type="text"
-                        variant="standard"
-                        label="image alt"
-                        {...register("image.alt")}
-                    />
-                    <span className="text-sm text-red-500">{errors.image?.alt?.message}</span>
+                    <div className="flex flex-col w-[100%]">
+                        <FloatingLabel className="dark:text-white"
+                            type="text"
+                            variant="standard"
+                            label="image alt"
+                            {...register("image.alt")}
+                        />
+                        <span className="text-sm text-red-500">{errors.image?.alt?.message}</span>
+                    </div>
                 </div>
 
                 <div className="flex flex-row gap-6 m-auto w-max">
-                    <FloatingLabel className="dark:text-white"
-                        type="text"
-                        variant="standard"
-                        label="state"
-                        {...register("address.state")}
-                    />
-                    <span className="text-sm text-red-500">{errors.address?.state?.message}</span>
+                    <div className="flex flex-col w-[100%]">
+                        <FloatingLabel className="dark:text-white"
+                            type="text"
+                            variant="standard"
+                            label="state"
+                            {...register("address.state")}
+                        />
+                        <span className="text-sm text-red-500">{errors.address?.state?.message}</span>
+                    </div>
 
-                    <FloatingLabel className="dark:text-white"
-                        type="text"
-                        variant="standard"
-                        label="country"
-                        {...register("address.country")}
-                    />
-                    <span className="text-sm text-red-500">{errors.address?.country?.message}</span>
+                    <div className="flex flex-col w-[100%]">
+                        <FloatingLabel className="dark:text-white"
+                            type="text"
+                            variant="standard"
+                            label="country"
+                            {...register("address.country")}
+                        />
+                        <span className="text-sm text-red-500">{errors.address?.country?.message}</span>
+                    </div>
                 </div>
 
                 <div className="flex flex-row gap-6 m-auto w-max">
-                    <FloatingLabel className="dark:text-white"
-                        type="text"
-                        variant="standard"
-                        label="city"
-                        {...register("address.city")}
-                    />
-                    <span className="text-sm text-red-500">{errors.address?.city?.message}</span>
+                    <div className="flex flex-col w-[100%]">
+                        <FloatingLabel className="dark:text-white"
+                            type="text"
+                            variant="standard"
+                            label="city"
+                            {...register("address.city")}
+                        />
+                        <span className="text-sm text-red-500">{errors.address?.city?.message}</span>
+                    </div>
 
-                    <FloatingLabel className="dark:text-white"
-                        type="text"
-                        variant="standard"
-                        label="street"
-                        {...register("address.street")}
-                    />
-                    <span className="text-sm text-red-500">{errors.address?.street?.message}</span>
+                    <div className="flex flex-col w-[100%]">
+                        <FloatingLabel className="dark:text-white"
+                            type="text"
+                            variant="standard"
+                            label="street"
+                            {...register("address.street")}
+                        />
+                        <span className="text-sm text-red-500">{errors.address?.street?.message}</span>
+                    </div>
                 </div>
 
                 <div className="flex flex-row gap-6 m-auto w-max">
-                    <FloatingLabel className="dark:text-white"
-                        type="number"
-                        variant="standard"
-                        label="houseNumber"
-                        {...register("address.houseNumber")}
-                    />
-                    <span className="text-sm text-red-500">{errors.address?.houseNumber?.message}</span>
+                    <div className="flex flex-col w-[100%]">
+                        <FloatingLabel className="dark:text-white"
+                            type="number"
+                            variant="standard"
+                            label="houseNumber"
+                            {...register("address.houseNumber")}
+                        />
+                        <span className="text-sm text-red-500">{errors.address?.houseNumber?.message}</span>
+                    </div>
 
-                    <FloatingLabel className="dark:text-white"
-                        type="number"
-                        variant="standard"
-                        label="zip"
-                        {...register("address.zip")}
-                    />
-                    <span className="text-sm text-red-500">{errors.address?.zip?.message}</span>
+                    <div className="flex flex-col w-[100%]">
+                        <FloatingLabel className="dark:text-white"
+                            type="number"
+                            variant="standard"
+                            label="zip"
+                            {...register("address.zip")}
+                        />
+                        <span className="text-sm text-red-500">{errors.address?.zip?.message}</span>
+                    </div>
                 </div>
 
                 <button className="h-10 text-sm text-white transition-colors rounded-md bg-neutral-500 hover:bg-neutral-600" onClick={navToMyCards}>Go Back</button>
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -12,11 +12,11 @@ const Card = () => {
     } = card();
 
     return (
-        <div className="flex flex-wrap w-[100vw]   justify-center bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700 gap-7 shadow">
+        <div className="flex flex-wrap w-[100vw]   justify-center bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700 gap-8 shadow">
             {
                 searchCards()!.map((card: TCard) => {
                     return (
-                        <div key={card._id} className="w-[365px] h-[550px] shadow-lg dark:shadow-xl dark:shadow-blue-500/50">
+                        <div key={card._id} className="w-[365px] h-[600px] shadow-lg dark:shadow-md dark:shadow-blue-500/50 flex justify-center items-center flex-col">
                             <img className="rounded-t-lg w-[100%] h-[200px] object-cover cursor-pointer" src={card.image.url} alt="image" onClick={() => navToCard(card._id)} />
                             <div className="p-5">
                                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{card.title}</h5>
@@ -24,7 +24,7 @@ const Card = () => {
                                 <hr />
                                 <p className="mt-3 mb-3 font-semibold text-gray-700 dark:text-gray-400">{card.phone}</p>
                                 <p className="mb-3 font-semibold text-gray-700 dark:text-gray-400">{card.email}</p>
-                                <p className="mb-3 font-semibold text-gray-700 dark:text-gray-400">{card.description}</p>
+                                <p className="mb-3 text-[14px] font-semibold text-gray-700 dark:text-gray-400">{card.description}</p>
                                 <br />
                                 {user.user && <FaHeart
                                     size={30}
