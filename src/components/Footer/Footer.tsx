61e9d4678d1805db4d6224144de9cf66845fd73c
ghost5554 <nick555310@gmail.com>
2024-10-13 15:52:04 +0300

    updated ui

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,20 +3,14 @@ const Footer = () => {
         <>
             <footer className="shadow bg-slate-200 dark:bg-gray-900">
                 <div className="w-full max-w-screen-xl p-4 mx-auto md:flex md:items-center md:justify-between">
-                    <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="/" className="hover:underline">Nikolas™</a>. All Rights Reserved.
+                    <span className="text-gray-500 text-md sm:text-center dark:text-gray-400">© 2024 <a href="/" className="hover:underline">Nikolas™</a>. All Rights Reserved.
                     </span>
-                    <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                    <ul className="flex flex-wrap items-center mt-3 font-semibold text-gray-500 text-md dark:text-gray-400 sm:mt-0">
                         <li>
                             <a href="/about" className="hover:underline me-4 md:me-6">About</a>
                         </li>
                         <li>
-                            <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
-                        </li>
-                        <li>
-                            <a href="#" className="hover:underline me-4 md:me-6">Licensing</a>
-                        </li>
-                        <li>
-                            <a href="#" className="hover:underline">Contact</a>
+                            <a href="https://mail.google.com/mail/u/0/#inbox" className="hover:underline">Contact</a>
                         </li>
                     </ul>
                 </div>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,59 +14,61 @@ const Header = () => {
     } = header();
 
     return (
-        <Navbar fluid className="list-none ">
+        <Navbar fluid className="list-none">
             <Navbar.Link as={Link} href="/" to="/" active={loc === '/'} className="flex flex-row gap-2">
                 <img src="../public/dark-mouse.jpeg" alt="dark-mouse" className="w-[30px] h-[30px]" />
-                <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">React Project</span>
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">React Project</span>
             </Navbar.Link>
 
             <Navbar.Toggle />
             <Navbar.Collapse>
-                <DarkThemeToggle />
+
+                <Navbar.Link as={Link} href="/" to="/" active={loc === '/'} className="text-lg ">
+                    Home
+                </Navbar.Link>
+
+                <Navbar.Link as={Link} href="/about" to="/about" active={loc === '/about'} className="text-lg">
+                    About
+                </Navbar.Link>
+
+                {user && (<Navbar.Link as={Link} href="/profile" to="/profile" active={loc === '/profile'} className="text-lg">
+                    Profile
+                </Navbar.Link>)}
+
+                {user && (<Navbar.Link as={Link} href="/favorites" to="/favorites" active={loc === '/favorites'} className="text-lg">
+                    Favorites
+                </Navbar.Link>)}
+
+                {user?.isBusiness && (<Navbar.Link as={Link} href="/mycards" to="/mycards" active={loc === '/mycards'} className="text-lg">
+                    My cards
+                </Navbar.Link>)}
+
+                {user?.isAdmin && (<Navbar.Link as={Link} href="/crm" to="/crm" active={loc === '/crm'} className="text-lg">
+                    Crm
+                </Navbar.Link>)}
+
                 <Navbar.Brand>
                     <TextInput rightIcon={FaSearch} onChange={search} />
                 </Navbar.Brand>
 
-                <Navbar.Link as={Link} href="/" to="/" active={loc === '/'}>
-                    Home
-                </Navbar.Link>
+                <DarkThemeToggle />
 
-                <Navbar.Link as={Link} href="/about" to="/about" active={loc === '/about'}>
-                    About
-                </Navbar.Link>
-
-                {user && (<Navbar.Link as={Link} href="/profile" to="/profile" active={loc === '/profile'}>
-                    Profile
-                </Navbar.Link>)}
-
-                {user && (<Navbar.Link as={Link} href="/favorites" to="/favorites" active={loc === '/favorites'}>
-                    Favorites
-                </Navbar.Link>)}
-
-                {user?.isBusiness && (<Navbar.Link as={Link} href="/mycards" to="/mycards" active={loc === '/mycards'}>
-                    My cards
-                </Navbar.Link>)}
-
-                {user?.isAdmin && (<Navbar.Link as={Link} href="/crm" to="/crm" active={loc === '/crm'}>
-                    Crm
-                </Navbar.Link>)}
-
-                {!user && (<Navbar.Link as={Link} href="/register" to="/register" active={loc === '/register'}>
+                {!user && (<Navbar.Link as={Link} href="/register" to="/register" active={loc === '/register'} className="text-lg">
                     Register
                 </Navbar.Link>)}
 
                 {!user && (
-                    <Navbar.Link as={Link} href="/login" to="/login" active={loc === '/login'}>
+                    <Navbar.Link as={Link} href="/login" to="/login" active={loc === '/login'} className="text-lg">
                         Login
                     </Navbar.Link>
                 )}
 
+
                 {user && (
-                    <Navbar.Link className="cursor-pointer" onClick={logout}>
+                    <Navbar.Link className="text-lg cursor-pointer" onClick={logout} >
                         Logout
                     </Navbar.Link>
                 )}
-
             </Navbar.Collapse>
 
         </Navbar>
